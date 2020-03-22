9018121c4a09c47fd40776a6e4be9bcdb7c51dbd
Ritesh Chauhan <chauhanx.ritesh@gmail.com>
2020-03-22 07:09:56 +0000

    bind to ldap server

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/go-ldap/ldap"
+)
+
+func main() {
+	l, err := ldap.Dial("tcp", fmt.Sprintf("%s:%d", "localhost", 389))
+	if err != nil {
+		log.Fatalf("Oops fatal error to dial to ldap server %s\n", err)
+	}
+
+	defer l.Close()
+
+	err = l.Bind("uid=chauhr,ou=People,dc=example,dc=com", "power")
+	if err != nil {
+		log.Fatalf("Error loging into the server %s\n", err)
+	}
+	fmt.Printf("Successful binding to server\n")
+}
